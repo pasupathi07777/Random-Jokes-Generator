c65ac422e9b7d3c91500b7a38f8bd84d8634fb7c
pasupathi07777 <pasupathi07777@gamil.com>
2024-08-14 15:51:30 +0530

    ll

--- src/component/Jock.js
+++ src/component/Jock.js
@@ -5,15 +5,21 @@ import { loder } from './loder'
 
 const Jock = () => {
     const [userInput, setUserInput] = useState("")
+    
+   
     const selector = useSelector((state) => state)
     const dispatch = useDispatch()
 
-
+   
     const omClicks = (e) => {
         dispatch(fetchData(userInput))
+       
+     
         setUserInput("")
+    
 
     }
+    
 
     const available = [
         "animal",
@@ -33,6 +39,8 @@ const Jock = () => {
         "sport",
         "travel"
     ]
+
+
     return (
         <div className="total w-[100vw] h-[100vh] px-4 py-2 bg-gray-200   flex flex-col items-center">
 
@@ -50,12 +58,12 @@ const Jock = () => {
 
             {selector.jockes.lodding ?loder : <p className={`para mt-3   text-center font-semibold ${selector.jockes.catogory ? "text-red-500 " : "text-black "} ${selector.jockes.result?"text-[24px]" : "text-[16px]"}  `}>{selector.jockes.joke}</p>}
 
-            <div className={`font-medium ${selector.jockes.catogory ? "flex" : "hidden"} text-center   flex-wrap justify-center mt-1 text-[19px] text-blue-700`}>
+            <div className={`font-medium ${selector.jockes.lodding?"hidden":"flex"} ${selector.jockes.catogory ? "flex" : "hidden"} text-center   flex-wrap justify-center mt-1 text-[19px] text-blue-700`}>
 
                 <p className=' font-semibold mr-2 text-black    text-center'>{`Available Lists are `} </p>
 
                 {available.map((e, i) => (
-                    <p onClick={()=>setUserInput(e)} className='text-[19px] cursor-pointer ' key={i}>{available.length-1===i?`${e}.`:`${e},`}</p>
+                    <p onClick={()=>setUserInput(e)} className='text-[19px] cursor-pointer  ' key={i}>{available.length-1===i?`${e}.`:`${e},`}</p>
 
                 ))}
 
